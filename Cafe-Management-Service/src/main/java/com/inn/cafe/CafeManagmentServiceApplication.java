46dbffb39cd828d92c2173a51eb9119cad39e31e
engmg99 <goyalmunish4306@gmail.com>
2023-11-27 16:35:09 +0530

    encrypted the username and password for email func

--- Cafe-Management-Service/src/main/java/com/inn/cafe/CafeManagmentServiceApplication.java
+++ Cafe-Management-Service/src/main/java/com/inn/cafe/CafeManagmentServiceApplication.java
@@ -3,7 +3,10 @@ package com.inn.cafe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class CafeManagmentServiceApplication {
 
 	public static void main(String[] args) {
